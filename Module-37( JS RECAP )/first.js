6c4ba7e8639f665dfdbab03a74bf25bf00674e73
Affnan Sawad <affnansawad2002@gmail.com>
2024-07-01 12:36:34 +0600

    six fundemantals of JS

--- Module-37( JS RECAP )/first.js
+++ Module-37( JS RECAP )/first.js
@@ -1,1 +1,16 @@
-J
+// Seven JavaScript Fundamentals That You Need To Know  :
+
+// 1. How to declare a variable . [ let , const , var]
+
+// 2 . conditions  , operators . [ if else , >= <= ,=== ,!== , <= , >=]
+
+//3. Multiple Conditions . [ if-else , else , && , || ]
+
+// 4. How to declare an ARRAY . [ length , indexof , push,pop,shift,unshift,includes etc.]
+
+
+//5.  Loop . [ for of , while , for , for in  ]
+
+// 6. function ..
+
+// 7.  Object ...
