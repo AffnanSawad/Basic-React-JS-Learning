ec81bac2693f7473cad92e65fe01a721766a2aa1
Affnan Sawad <affnansawad2002@gmail.com>
2024-07-04 13:00:07 +0600

    Practice Project of This Milestone

--- knowledge-Cafe-M-42/src/components/addedBookmark/AddedBookmark.jsx
+++ knowledge-Cafe-M-42/src/components/addedBookmark/AddedBookmark.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const AddedBookmark = ({addedBookmark}) => {
 
-    const {title} = addedBookmark;
+    const {title,ab} = addedBookmark;
     return (
         <div>
             <h3 className='text-xl font-bold text-red-600 mt-5 mb-2'> {title} </h3>
